46775e828525f7a6b2fcb03779737b72ca229d6d
maa000 <hesuke123@gmail.com>
2024-09-22 14:18:07 +0200

    kommentezes

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //ez egy hiba
+    std::cout << '1-100 ertekek duplazasa' //ez egy hiba
+    for (int i = 0;)  // ez egy hiba
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // ez egy hiba
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //ez egy hiba
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //ez egy hiba
+    for (int i = 0; i < N_ELEMENTS, i++)  // ez egy hiba
     {
-        atlag += b[i]
+        atlag += b[i] //ez egy hiba
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
